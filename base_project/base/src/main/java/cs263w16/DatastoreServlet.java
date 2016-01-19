3456d9a0d0fbb3e7c249081f1946fb5a07e4bc4d
Francois Malinowski <francois@francois-malinowski.fr>
2016-01-18 18:15:31 -0800

    datastore step 3

--- base_project/base/src/main/java/cs263w16/DatastoreServlet.java
+++ base_project/base/src/main/java/cs263w16/DatastoreServlet.java
@@ -2,9 +2,11 @@ package cs263w16;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+
 import java.io.*;
 import java.util.*;
 import java.util.logging.*;
+
 import com.google.appengine.api.datastore.*;
 import com.google.appengine.api.memcache.*;
 
@@ -13,12 +15,58 @@ public class DatastoreServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-      resp.setContentType("text/html");
+	  DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+	  
+	  resp.setContentType("text/html");
       resp.getWriter().println("<html><body>");
-      resp.getWriter().println("<h2>Hello World</h2>"); //remove this line
-
-      //Add your code here
-
-      resp.getWriter().println("</body></html>");
+	  
+	  // No parameters are passed in
+	  if(!req.getParameterNames().hasMoreElements()) {
+		  Query allTasksQuery = new Query("TaskData");
+		  PreparedQuery preparedQuery = datastore.prepare(allTasksQuery);
+		  
+		  resp.getWriter().println("<h1>Here are all the TaskData</h1>");
+		  
+		  for (Entity result : preparedQuery.asIterable()) {
+			  String keyname = (String) result.getKey().getName();
+			  String value = (String) result.getProperty("value");
+			  Date date = (Date) result.getProperty("date");
+			  resp.getWriter().println(keyname+ " - " + value + " - " + date + "<br>");
+		  }
+	  }
+	  else if (req.getParameter("keyname") != null && !req.getParameter("keyname").isEmpty() && req.getParameter("value") == null) {
+		  Key taskKey = KeyFactory.createKey("TaskData", req.getParameter("keyname"));
+		  Entity taskResult;
+		  
+		  resp.getWriter().println("<h1>TaskData for " + req.getParameter("keyname") + "</h1>");
+		  
+		  try {
+			  taskResult = datastore.get(taskKey);
+			  String value = (String) taskResult.getProperty("value");
+			  Date date = (Date) taskResult.getProperty("date");
+			  resp.getWriter().println(value + " - " + date + "<br><br>");
+		  } catch (EntityNotFoundException e) {
+			  resp.getWriter().println("No entities found");
+		  }
+		  
+		  
+	  } 
+	  else if (req.getParameter("keyname") != null & !req.getParameter("keyname").isEmpty() && req.getParameter("value") != null) {
+		  String keyname = req.getParameter("keyname");
+		  String value = req.getParameter("value");
+		  Entity task = new Entity("TaskData", keyname);
+		  task.setProperty("value", value);
+		  task.setProperty("date", new Date());
+		  
+		  datastore.put(task);
+		  
+		  resp.getWriter().println("Stored " + keyname + " and " + value + " in Datastore");
+	  }
+	  else {
+		  resp.getWriter().println("This action doesn't work man!");
+	  }
+	  
+	  
+	  resp.getWriter().println("</body></html>");
   }
 }
