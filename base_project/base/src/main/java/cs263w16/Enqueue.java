3c221271d9dc5c3a3f2dc662274e7d8d1dc5d264
Francois Malinowski <francois@francois-malinowski.fr>
2016-01-18 21:21:04 -0800

    fm - add value in task. Created a task entity when using worker

--- base_project/base/src/main/java/cs263w16/Enqueue.java
+++ base_project/base/src/main/java/cs263w16/Enqueue.java
@@ -15,10 +15,11 @@ public class Enqueue extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String key = request.getParameter("keyname");
+        String value = request.getParameter("value");
 
         // Add the task to the default queue.
         Queue queue = QueueFactory.getDefaultQueue();
-        queue.add(TaskOptions.Builder.withUrl("/worker").param("keyname", key));
+        queue.add(TaskOptions.Builder.withUrl("/worker").param("keyname", key).param("value", value));
 
         response.sendRedirect("/done.html");
     }
--- base_project/base/src/main/java/cs263w16/Worker.java
+++ base_project/base/src/main/java/cs263w16/Worker.java
@@ -1,15 +1,32 @@
 package cs263w16;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
 public class Worker extends HttpServlet {
- protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
          throws ServletException, IOException {
-     String key = request.getParameter("keyname");
-     // Do something with key.
- }
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		
+		String keyname = request.getParameter("keyname");
+		String value = request.getParameter("value");
+     
+		if (keyname != null && value != null) {
+			Entity task = new Entity("TaskData", keyname);
+			task.setProperty("value", value);
+			task.setProperty("date", new Date());
+		  
+			datastore.put(task);
+		}
+    
+	}
 }
